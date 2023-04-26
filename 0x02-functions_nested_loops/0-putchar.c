9cae6825ebdb7e2d86a73aa8887c88f096324036
Gloria John <glogloriajohn@example.com>
2023-04-25 20:46:30 -0700

    Write a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Prints _putchar as a message.
  *
@@ -13,10 +13,10 @@ int main(void)
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
-		putchar(str[count]);
+		_putchar(str[count]);
 	}
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
 
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+include <unistd.h>
+/**
+ * _putchar - write c to stdout
+ * @c - to print
+ *
+ * Return: 0;
+ * -1 is returned on error and set appropriately.
+ */
+
+int _putchar(char c)
+{
+
+return (write(1,&c,1));
+}
