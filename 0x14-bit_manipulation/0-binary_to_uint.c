22d91c233c21452b7710182dccb8d78d33ad6a00
Gloria John <glogloriajohn@gmail.com>
2023-06-08 16:06:56 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,19 @@
  *
  * Return: the converted unsigned integer
  */
-unsigned int binary_to_uint(const char *binary)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int decimal_value = 0;
 
-	if (!binary)
+	if (!b)
 		return (0);
 
-	for (i = 0; binary[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (binary[i] < '0' || binary[i] > '1')
+		if (b[i] < '0' || binary[i] > '1')
 			return (0);
-		decimal_value = 2 * decimal_value + (binary[i] - '0');
+		decimal_value = 2 * decimal_value + (b[i] - '0');
 	}
 
 	return (decimal_value);
